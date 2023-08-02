ee7d9649190219777ee8d02c24c6b5a03ec6ac81
katerega <8218kelmo@gmail.com>
2023-08-02 05:20:43 +0300

    Create arb2.sol

---
+++ Blockchain/arb2.sol
@@ -0,0 +1,152 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+interface IUniswapV2Router {
+  function getAmountsOut(uint256 amountIn, address[] memory path) external view returns (uint256[] memory amounts);
+  function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) external returns (uint256[] memory amounts);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+  function token1() external view returns (address);
+  function swap(uint256 amount0Out,	uint256 amount1Out,	address to,	bytes calldata data) external;
+}
+
+contract Arb is Ownable {
+
+
+    /*
+    	function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount) private {
+		IERC20(_tokenIn).approve(router, _amount);
+		address[] memory path;
+		path = new address[](2);
+		path[0] = _tokenIn;
+		path[1] = _tokenOut;
+		uint deadline = block.timestamp + 300;
+		IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1, path, address(this), deadline);
+	}
+    */
+
+  
+/*
+  function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount, uint256 _amountOutMin) private {
+    IERC20(_tokenIn).approve(router, _amount);
+    address[] memory path;
+    path = new address[](2);
+    path[0] = _tokenIn;
+    path[1] = _tokenOut;
+    uint deadline = block.timestamp + 300;
+    IUniswapV2Router(router).swapExactTokensForTokens(_amount, _amountOutMin, path, address(this), deadline);
+}
+*/
+
+function swap(address router, address _tokenIn, address _tokenOut, uint256 _amount, uint256 _amountOutMin) private {
+    // Check that the contract has enough allowance to spend _amount of _tokenIn
+    require(IERC20(_tokenIn).allowance(address(this), router) >= _amount, "Insufficient allowance");
+
+    // Check that the contract has enough balance of _tokenIn to cover the swap
+    require(IERC20(_tokenIn).balanceOf(address(this)) >= _amount, "Insufficient balance");
+
+    // Approve the router to spend the _amount of _tokenIn
+    IERC20(_tokenIn).approve(router, _amount);
+
+    address[] memory path = new address[](2);
+    path[0] = _tokenIn;
+    path[1] = _tokenOut;
+
+    uint deadline = block.timestamp + 300;
+
+    // Perform the swap_amountOutMin
+    IUniswapV2Router(router).swapExactTokensForTokens(_amount, 1000000, path, address(this), deadline);
+}
+
+
+	function getAmountOutMin(address router, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256) {
+		address[] memory path;
+		path = new address[](2);
+		path[0] = _tokenIn;
+		path[1] = _tokenOut;
+		uint256[] memory amountOutMins = IUniswapV2Router(router).getAmountsOut(_amount, path);
+		return amountOutMins[path.length -1];
+	}
+
+  function estimateDualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external view returns (uint256) {
+		uint256 amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
+		uint256 amtBack2 = getAmountOutMin(_router2, _token2, _token1, amtBack1);
+		return amtBack2;
+	}
+	
+  
+  /*
+  function dualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount) external onlyOwner {
+    uint startBalance = IERC20(_token1).balanceOf(address(this));
+    uint token2InitialBalance = IERC20(_token2).balanceOf(address(this));
+    swap(_router1,_token1, _token2,_amount);
+    uint token2Balance = IERC20(_token2).balanceOf(address(this));
+    uint tradeableAmount = token2Balance - token2InitialBalance;
+    swap(_router2,_token2, _token1,tradeableAmount);
+    uint endBalance = IERC20(_token1).balanceOf(address(this));
+    require(endBalance > startBalance, "Trade Reverted, No Profit Made");
+  }
+  */
+  function dualDexTrade(address _router1, address _router2, address _token1, address _token2, uint256 _amount, uint256 _slippageTolerance) external onlyOwner {
+    uint startBalance = IERC20(_token1).balanceOf(address(this));
+    uint token2InitialBalance = IERC20(_token2).balanceOf(address(this));
+
+    // Perform the first swap from token1 to token2 on router1 with slippage tolerance
+    uint256 amountOutMin1 = (_amount * (10000 - _slippageTolerance)) / 10000;
+    swap(_router1, _token1, _token2, _amount, amountOutMin1);
+
+    uint token2Balance = IERC20(_token2).balanceOf(address(this));
+    uint tradeableAmount = token2Balance - token2InitialBalance;
+
+    // Check if the trade results in a profit
+    require(tradeableAmount > 0, "No profit opportunity");
+
+    // Perform the second swap from token2 to token1 on router2 with slippage tolerance
+    uint256 amountOutMin2 = (tradeableAmount * (10000 - _slippageTolerance)) / 10000;
+    swap(_router2, _token2, _token1, tradeableAmount, amountOutMin2);
+
+    uint endBalance = IERC20(_token1).balanceOf(address(this));
+
+    // Check if a profit is made
+    require(endBalance > startBalance, "Trade Reverted, No Profit Made");
+}
+
+
+
+
+
+	function estimateTriDexTrade(address _router1, address _router2, address _router3, address _token1, address _token2, address _token3, uint256 _amount) external view returns (uint256) {
+		uint amtBack1 = getAmountOutMin(_router1, _token1, _token2, _amount);
+		uint amtBack2 = getAmountOutMin(_router2, _token2, _token3, amtBack1);
+		uint amtBack3 = getAmountOutMin(_router3, _token3, _token1, amtBack2);
+		return amtBack3;
+	}
+
+	function getBalance (address _tokenContractAddress) external view  returns (uint256) {
+		uint balance = IERC20(_tokenContractAddress).balanceOf(address(this));
+		return balance;
+	}
+
+	function recoverEth() external onlyOwner {
+		payable(msg.sender).transfer(address(this).balance);
+	}
+
+	function recoverTokens(address tokenAddress) external onlyOwner {
+		IERC20 token = IERC20(tokenAddress);
+		uint256 amount = token.balanceOf(address(this));
+		console.log(
+			"Transferring %s %s tokens",
+			msg.sender,
+			amount
+		);
+		token.transfer(msg.sender, amount);
+	}
+
+}
